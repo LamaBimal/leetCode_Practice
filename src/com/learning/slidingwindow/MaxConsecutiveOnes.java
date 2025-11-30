7f67ee8f651840854cc127eeb29243180098b442
Bimal Tamang <bimaltamang1234@gmail.com>
2025-11-30 21:57:24 +0545

    Max consecutive ones

--- src/com/learning/slidingwindow/MaxConsecutiveOnes.java
+++ src/com/learning/slidingwindow/MaxConsecutiveOnes.java
@@ -20,7 +20,34 @@ package com.learning.slidingwindow;
  */
 
 public class MaxConsecutiveOnes {
-    public static void main(String[] args) {
 
+    public int findLongestOnes(int [] numbers, int maxZeros){
+        int left =0;
+        int right =0;
+        int zerosCount = 0;
+        int maxOnes = 0;
+
+        for(int i=0;i< numbers.length; i++){
+
+            if(numbers[i]==0) zerosCount++;
+
+            while(zerosCount> maxZeros){
+                if(numbers[left]==0)
+                    zerosCount--;
+                left++;
+            }
+            maxOnes = Math.max(maxOnes, right-left+1);
+            right++;
+
+        }
+
+        return maxOnes;
+    }
+
+    public static void main(String[] args) {
+        MaxConsecutiveOnes maxConsecutiveOnes = new MaxConsecutiveOnes();
+        int [] input = new int[]{1,0,1,0,1,0,1,0,1,1};
+        int maxValue = maxConsecutiveOnes.findLongestOnes(input,3);
+        System.out.println("Maximum Ones length is "+maxValue);
     }
 }
